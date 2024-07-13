c95c02cd8313033ed3dd6091bfdeb1a127effd5a
Arthur de Oliveira Batista <arthurdeol@gmail.com>
2024-07-13 11:49:49 -0400

    hotfix: fixing fetch

--- src/pages/Praise/index.jsx
+++ src/pages/Praise/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import axios from "axios";
 import { Container } from "./styles";
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Header from "../../components/Header";
+import Stack from "@mui/material/Stack";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function Praise() {
   let [louvor, setLouvor] = useState("");
@@ -15,44 +16,50 @@ export default function Praise() {
   const activeTab = iconName;
 
   useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        setLouvor(data);
-        if (activeTab === "LuType") {
-          setIframeUrl(
-            "data:" +
-              data.lyricsPdf.contentType +
-              ";base64," +
-              data.lyricsPdf.file
-          );
-        } else if (activeTab === "LuListMusic") {
-          setIframeUrl(
-            "data:" +
-              data.chordsPdf.contentType +
-              ";base64," +
-              data.chordsPdf.file
-          );
-        } else if (activeTab === "LuMusic") {
-          setIframeUrl(
-            "data:" +
-              data.sheetMusicPdf.contentType +
-              ";base64," +
-              data.sheetMusicPdf.file
-          );
-        } else if (activeTab === "LuVolume1") {
-          setIframeUrl(
-            "data:" +
-              data.audioFile.contentType +
-              ";base64," +
-              data.audioFile.file
-          );
-        }
-      })
-      .catch((err) => console.error(err));
+    async function fetchData() {
+      const response = await fetch(url);
+      const louvor = await response.json();
+      setLouvor(response.data);
+      if (louvor) {
+        setActiveTab(iconName, louvor);
+      } else {
+        console.log("praise not found");
+      }
+    }
+    fetchData();
   }, [iconName]);
 
-  // console.log("louvor", louvor);
+  function setActiveTab(activeTab, louvor) {
+    if (activeTab === "LuType") {
+      setIframeUrl(
+        "data:" +
+          louvor.lyricsPdf.contentType +
+          ";base64," +
+          louvor.lyricsPdf.file
+      );
+    } else if (activeTab === "LuListMusic") {
+      setIframeUrl(
+        "data:" +
+          louvor.chordsPdf.contentType +
+          ";base64," +
+          louvor.chordsPdf.file
+      );
+    } else if (activeTab === "LuMusic") {
+      setIframeUrl(
+        "data:" +
+          louvor.sheetMusicPdf.contentType +
+          ";base64," +
+          louvor.sheetMusicPdf.file
+      );
+    } else if (activeTab === "LuVolume1") {
+      setIframeUrl(
+        "data:" +
+          louvor.audioFile.contentType +
+          ";base64," +
+          louvor.audioFile.file
+      );
+    }
+  }
 
   return (
     <Container>
