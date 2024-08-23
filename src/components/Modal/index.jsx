60c384970624121e359b0056cb4df2dccfc1e214
SamaraCollet <samaracollet@gmail.com>
2024-08-23 18:11:12 -0400

    add complex filter, tag CIA

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,20 +1,20 @@
-import { Container } from "./styles";
 import Box from "@mui/material/Box";
 import { LuX } from "react-icons/lu";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import Button from "@mui/material/Button";
 import Checkbox from "@mui/material/Checkbox";
 import { red, grey } from "@mui/material/colors";
 import { useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: "auto",
   height: "auto",
   bgcolor: "background.paper",
   border: "1px solid #ededed",
@@ -22,7 +22,30 @@ const style = {
   boxShadow: 24,
   color: "black",
   p: 4,
+  width: { xs: "90%", sm: "70%", md: "50%", lg: "auto" },
 };
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "Nunito",
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+    ].join(","),
+  },
+
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 500,
+      md: 768,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
 
 const xClose = {
   position: "absolute",
@@ -33,6 +56,7 @@ const xClose = {
 const footerFilter = {
   display: "flex",
   justifyContent: "end",
+  marginTop: "10px",
 };
 
 const checked = {
@@ -42,35 +66,126 @@ const checked = {
   },
 };
 
-export default function BasicModal({ openModal, onCloseModal }) {
+const themeTitle = {
+  margin: "10px 0 8px",
+  color: "grey.700",
+  fontSize: "16px",
+};
+
+const title = {
+  margin: "0 0 20px",
+  color: "grey.700",
+};
+
+const button = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  background: "#fff",
+  borderRadius: "10px",
+  borderColor: "grey.800",
+  color: "grey.800",
+  textTransform: "none",
+  fontSize: "16px",
+  marginRight: "5px",
+};
+
+export default function BasicModal({
+  openModal,
+  onCloseModal,
+  setComplexFilter,
+}) {
   const [formValue, setFormValue] = useState({
-    containsInSongBook: true,
-    containsInSuplementareASongBook: true,
-    containsInSuplementareBSongBook: true,
-    containsInCiasSongBook: true,
+    containsInCiasSongBook: false,
+    pleadingTheme: false,
+    chorusesTheme: false,
+    confortAndEncouragementTheme: false,
+    dedicationTheme: false,
+    dethRessurectionAndSalvationTheme: false,
+    eternityAndTheReturnOfJesusTheme: false,
+    invocationFellowshipTheme: false,
+    santificationOutpouringHSTheme: false,
+    praiseGroupTheme: false,
+    praiseTheme: false,
   });
+
+  const {
+    containsInCiasSongBook,
+    pleadingTheme,
+    chorusesTheme,
+    confortAndEncouragementTheme,
+    dedicationTheme,
+    dethRessurectionAndSalvationTheme,
+    eternityAndTheReturnOfJesusTheme,
+    invocationFellowshipTheme,
+    santificationOutpouringHSTheme,
+    praiseGroupTheme,
+    praiseTheme,
+  } = formValue;
 
   const handleChange = (event) => {
     setFormValue({
       ...formValue,
       [event.target.name]: event.target.checked,
     });
-    // console.log(formValue);
   };
 
-  const {
-    containsInSongBook,
-    containsInSuplementareASongBook,
-    containsInSuplementareBSongBook,
-    containsInCiasSongBook,
-  } = formValue;
-
   function applyFilter() {
-    console.log(formValue);
+    let themesApplied = [];
+    if (formValue) {
+      if (formValue.pleadingTheme) {
+        themesApplied.push("PLEADING");
+      }
+      if (formValue.chorusesTheme) {
+        themesApplied.push("CHORUSES");
+      }
+      if (formValue.confortAndEncouragementTheme) {
+        themesApplied.push("CONFORT AND ENCOURAGEMENT");
+      }
+      if (formValue.dedicationTheme) {
+        themesApplied.push("DEDICATION");
+      }
+      if (formValue.dethRessurectionAndSalvationTheme) {
+        themesApplied.push("DETH, RESSURECTION AND SALVATION");
+      }
+      if (formValue.eternityAndTheReturnOfJesusTheme) {
+        themesApplied.push("ETERNITY AND THE RETURN OF JESUS");
+      }
+      if (formValue.invocationFellowshipTheme) {
+        themesApplied.push("INVOCATION FELLOWSHIP");
+      }
+      if (formValue.santificationOutpouringHSTheme) {
+        themesApplied.push("SANTIFICATION AND OUTPOURING OF THE HOLY SPIRIT");
+      }
+      if (formValue.praiseGroupTheme) {
+        themesApplied.push("PRAISE GROUP");
+      }
+      if (formValue.praiseTheme) {
+        themesApplied.push("PRAISE");
+      }
+    }
+    onCloseModal();
+    setComplexFilter(formValue, themesApplied);
+  }
+
+  function cleanFilter() {
+    setFormValue({
+      containsInCiasSongBook: false,
+      pleadingTheme: false,
+      chorusesTheme: false,
+      confortAndEncouragementTheme: false,
+      dedicationTheme: false,
+      dethRessurectionAndSalvationTheme: false,
+      eternityAndTheReturnOfJesusTheme: false,
+      invocationFellowshipTheme: false,
+      santificationOutpouringHSTheme: false,
+      praiseGroupTheme: false,
+      praiseTheme: false,
+    });
   }
 
   return (
-    <Container>
+    <ThemeProvider theme={theme}>
       <Modal
         open={openModal}
         aria-labelledby="modal-modal-title"
@@ -81,46 +196,16 @@ export default function BasicModal({ openModal, onCloseModal }) {
             <LuX />
           </Box>
 
-          <Typography id="modal-modal-title" variant="h6" component="h2">
+          <Typography
+            sx={title}
+            id="modal-modal-title"
+            variant="h5"
+            component="h2"
+          >
             Filter
           </Typography>
 
           <FormGroup>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  sx={checked}
-                  checked={containsInSongBook}
-                  onChange={handleChange}
-                  name="containsInSongBook"
-                />
-              }
-              label="Song Book"
-            />
-
-            <FormControlLabel
-              control={
-                <Checkbox
-                  sx={checked}
-                  checked={containsInSuplementareASongBook}
-                  onChange={handleChange}
-                  name="containsInSuplementareASongBook"
-                />
-              }
-              label="Suplementare A"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  sx={checked}
-                  checked={containsInSuplementareBSongBook}
-                  onChange={handleChange}
-                  name="containsInSuplementareBSongBook"
-                />
-              }
-              label="Suplementare B"
-            />
-
             <FormControlLabel
               control={
                 <Checkbox
@@ -132,13 +217,16 @@ export default function BasicModal({ openModal, onCloseModal }) {
               }
               label="CIA's"
             />
-            {/* <FormControlLabel
+
+            <Typography sx={themeTitle}>Theme:</Typography>
+
+            <FormControlLabel
               control={
                 <Checkbox
                   sx={checked}
-                  checked={songBook}
+                  checked={pleadingTheme}
                   onChange={handleChange}
-                  name="songBook"
+                  name="pleadingTheme"
                 />
               }
               label="Pleading"
@@ -147,9 +235,31 @@ export default function BasicModal({ openModal, onCloseModal }) {
               control={
                 <Checkbox
                   sx={checked}
-                  checked={songBook}
+                  checked={confortAndEncouragementTheme}
                   onChange={handleChange}
-                  name="songBook"
+                  name="confortAndEncouragementTheme"
+                />
+              }
+              label="Comfort and Encouragement"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  sx={checked}
+                  checked={dedicationTheme}
+                  onChange={handleChange}
+                  name="dedicationTheme"
+                />
+              }
+              label="Dedication"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  sx={checked}
+                  checked={invocationFellowshipTheme}
+                  onChange={handleChange}
+                  name="invocationFellowshipTheme"
                 />
               }
               label="Invocation and Fellowship"
@@ -159,42 +269,21 @@ export default function BasicModal({ openModal, onCloseModal }) {
               control={
                 <Checkbox
                   sx={checked}
-                  checked={songBook}
+                  checked={dethRessurectionAndSalvationTheme}
                   onChange={handleChange}
-                  name="songBook"
+                  name="dethRessurectionAndSalvationTheme"
                 />
               }
               label="Death, Resurrection and Salvation"
             />
+
             <FormControlLabel
               control={
                 <Checkbox
                   sx={checked}
-                  checked={songBook}
+                  checked={santificationOutpouringHSTheme}
                   onChange={handleChange}
-                  name="songBook"
-                />
-              }
-              label="Dedication"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  sx={checked}
-                  checked={songBook}
-                  onChange={handleChange}
-                  name="songBook"
-                />
-              }
-              label="Comfort and Encouragement"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  sx={checked}
-                  checked={songBook}
-                  onChange={handleChange}
-                  name="songBook"
+                  name="santificationOutpouringHSTheme"
                 />
               }
               label="Santification and Outpouring of the Holy Spirit"
@@ -203,9 +292,9 @@ export default function BasicModal({ openModal, onCloseModal }) {
               control={
                 <Checkbox
                   sx={checked}
-                  checked={songBook}
+                  checked={eternityAndTheReturnOfJesusTheme}
                   onChange={handleChange}
-                  name="songBook"
+                  name="eternityAndTheReturnOfJesusTheme"
                 />
               }
               label="Eternity and the Return of Jesus"
@@ -214,9 +303,9 @@ export default function BasicModal({ openModal, onCloseModal }) {
               control={
                 <Checkbox
                   sx={checked}
-                  checked={songBook}
+                  checked={praiseTheme}
                   onChange={handleChange}
-                  name="songBook"
+                  name="praiseTheme"
                 />
               }
               label="Praise"
@@ -225,26 +314,49 @@ export default function BasicModal({ openModal, onCloseModal }) {
               control={
                 <Checkbox
                   sx={checked}
-                  checked={songBook}
+                  checked={praiseGroupTheme}
                   onChange={handleChange}
-                  name="songBook"
+                  name="praiseGroupTheme"
                 />
               }
               label="Praise Group"
-            /> */}
-            {/* <FormControlLabel
-              control={<Checkbox defaultChecked sx={checked} />}
-              label="Suplementare A"
-            /> */}
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  sx={checked}
+                  checked={chorusesTheme}
+                  onChange={handleChange}
+                  name="chorusesTheme"
+                />
+              }
+              label="Choruses"
+            />
             <Box sx={footerFilter}>
-              <button>Undo</button>
-              <button type="submit" onClick={applyFilter}>
-                Apply
-              </button>
+              <Button
+                variant="outlined"
+                type="button"
+                size="small"
+                color="error"
+                onClick={cleanFilter}
+                sx={button}
+              >
+                Unselect
+              </Button>
+              <Button
+                variant="outlined"
+                size="small"
+                type="submit"
+                color="error"
+                onClick={applyFilter}
+                sx={button}
+              >
+                Apply filter
+              </Button>
             </Box>
           </FormGroup>
         </Box>
       </Modal>
-    </Container>
+    </ThemeProvider>
   );
 }
