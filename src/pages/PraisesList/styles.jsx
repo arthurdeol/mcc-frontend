3b9bc2db53c0577c88194916070b0cefd8cd757b
SamaraCollet <samaracollet@gmail.com>
2024-09-15 00:57:55 -0400

    height

--- src/pages/PraisesList/styles.jsx
+++ src/pages/PraisesList/styles.jsx
@@ -60,7 +60,7 @@ export const ContainerPraisesList = styled.div`
 
   @media (max-width: 768px) {
     .main-container {
-      height: 90vh;
+      height: 85vh;
       margin: 0;
       box-shadow: none;
       border-radius: 0;
